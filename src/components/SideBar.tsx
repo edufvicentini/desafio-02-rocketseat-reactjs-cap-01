501b7a10eab8f35a9724d6872d79462893996752
Eduardo Frota Vicentini <eduardofvicentini@gmail.com>
2022-03-29 00:42:51 -0300

    Yea, it's working...

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,72 @@
+import { useState, useEffect } from "react";
+import { Button } from "./Button";
+import { api } from '../services/api';
+import { Content } from "./Content";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface Movie {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
 export function SideBar() {
-  // Complete aqui
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    api.get<Movie[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+
+
+  return (
+  <div style={{ display: 'flex', flexDirection: 'row' }}>
+  <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  </nav>
+
+  <Content movies={movies} selectedGenre={selectedGenre}/>
+
+  </div>
+
+)
 }
